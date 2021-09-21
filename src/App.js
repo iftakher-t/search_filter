1f0863e339b9783061a84cb54e50f52662e6ce47
Md Iftakher Hossain <hmdiftakher@gmail.com>
2021-09-21 21:13:47 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './components/Navbar/Navbar'
+import {BrowserRouter} from 'react-router-dom';
+
+
+function App() {
+  return (
+
+    <div className="App">
+        <Main />
+    </div>
+  );
+} 
+
+export default App;
